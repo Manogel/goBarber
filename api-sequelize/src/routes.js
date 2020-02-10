eaa6721e72145107a714eb2cdd61e6abf686ce01
Manogel <manoel_ggomes@hotmail.com>
2020-02-10 11:41:10 -0300

    index appointments with filter

--- api-sequelize/src/routes.js
+++ api-sequelize/src/routes.js
@@ -24,6 +24,7 @@ routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
 
 routes.put('/users', UserController.update);
 
