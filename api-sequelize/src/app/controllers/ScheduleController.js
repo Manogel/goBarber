4ce099fef058ea1aa76cd216f9e547113d1495f1
Manogel <manoel_ggomes@hotmail.com>
2020-02-10 12:01:35 -0300

    filter appointment of day

---
+++ api-sequelize/src/app/controllers/ScheduleController.js
@@ -0,0 +1,37 @@
+import * as Yup from 'yup';
+import { Op } from 'sequelize';
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+import File from '../models/File';
+
+class ScheduleController {
+  async index(req, res) {
+    // const { page = 1 } = req.query;
+    const isProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!isProvider) {
+      res.status(401).json({ error: 'User is not a provider!' });
+    }
+
+    const { date } = req.query;
+    const parseDate = parseISO(date);
+
+    const appointment = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)],
+        },
+      },
+      order: ['date'],
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new ScheduleController();
