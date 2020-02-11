e84ed2060a675c08bb4e8f3eea00b1c5fd588a24
Manogel <manoel_ggomes@hotmail.com>
2020-02-11 10:37:09 -0300

    Create notification

---
+++ api-sequelize/src/app/controllers/NotificationController.js
@@ -0,0 +1,48 @@
+import * as Yup from 'yup';
+import { Op } from 'sequelize';
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+import Notification from '../schemas/notification';
+
+class NotificationController {
+  async index(req, res) {
+    const isProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!isProvider) {
+      res.status(401).json({ error: 'User is not a provider!' });
+    }
+
+    const notifications = await Notification.find({
+      user: req.userId,
+    })
+      .sort({ createdAt: -1 })
+      .limit(20);
+
+    return res.json(notifications);
+  }
+
+  async update(req, res) {
+    const isProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!isProvider) {
+      res.status(401).json({ error: 'User is not a provider!' });
+    }
+
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      {
+        read: true,
+      },
+      { new: true }
+    );
+
+    return res.json(notification);
+  }
+}
+
+export default new NotificationController();
