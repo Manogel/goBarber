3cf762fe5dd39b0b9524ac562df44535f96f6d11
Manogel <manoel_ggomes@hotmail.com>
2020-02-11 11:28:10 -0300

    nodemailer configuration

---
+++ api-sequelize/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '9131c264d77c78',
+    pass: 'a36dc1fac9fb5b',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
