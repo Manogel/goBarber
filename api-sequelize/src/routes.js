417f24ce730993d55168128bd1f963526bd5b90a
Manogel <manoel_ggomes@hotmail.com>
2020-02-12 11:59:49 -0300

    Hours available of provider

--- api-sequelize/src/routes.js
+++ api-sequelize/src/routes.js
@@ -8,6 +8,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMiddleware from './app/middlewares/auth';
 import uploadConfig from './config/upload';
@@ -25,6 +26,8 @@ routes.use(authMiddleware);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
 routes.delete('/appointments/:id', AppointmentController.destroy);
