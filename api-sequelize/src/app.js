27ba5ddcaf88592c68270b862121e4bc2013d8c5
Manogel <manoel_ggomes@hotmail.com>
2020-02-14 11:19:34 -0300

    configure environment

--- api-sequelize/src/app.js
+++ api-sequelize/src/app.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import Youch from 'youch';
@@ -5,7 +6,6 @@ import * as Sentry from '@sentry/node';
 import sentryConfig from './config/sentry';
 import 'express-async-errors';
 import routes from './routes';
-import 'dotenv/config';
 import './database';
 
 class App {
@@ -35,8 +35,11 @@ class App {
 
   exceptionHandler() {
     this.server.use(async (err, req, res, next) => {
-      const errors = await new Youch(err, req).toJSON();
-      return res.status(500).json(errors);
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+        return res.status(500).json(errors);
+      }
+      return res.status(500).json({ error: 'Internal server error' });
     });
   }
 }
--- api-sequelize/src/config/mail.js
+++ api-sequelize/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '9131c264d77c78',
-    pass: 'a36dc1fac9fb5b',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe Gobarber <noreply@gobarber.com>',
