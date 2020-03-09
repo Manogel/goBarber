7cd07fd111a43f542a21ef9875326c5cce790bb9
Manogel <manoel_ggomes@hotmail.com>
2020-03-09 11:28:49 -0300

    babel eslint root import

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Route';
-import SignIn from '../pages/SignIn';
+import SignIn from '~/pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
