07553d0ce81d60e6b442dd36ef70cca47aaf65ea
Manogel <manoel_ggomes@hotmail.com>
2020-04-02 11:35:37 -0300

    dashboard page ok

---
+++ web/src/pages/Dashboard/styles.js
@@ -0,0 +1,52 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 600px;
+  margin: 50px auto 0;
+
+  display: flex;
+  flex-direction: column;
+
+  header {
+    display: flex;
+    align-self: center;
+    align-items: center;
+
+    button {
+      background: none;
+    }
+
+    strong {
+      color: #fff;
+      font-size: 24px;
+      margin: 0 15px;
+    }
+  }
+
+  ul {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 15px;
+    margin-top: 30px;
+  }
+`;
+
+export const Time = styled.li`
+  padding: 20px;
+  border-radius: 4px;
+  background: #fff;
+  opacity: ${({ past }) => (past ? 0.6 : 1)};
+
+  strong {
+    display: block;
+    color: ${({ available }) => (available ? '#999' : '#7159c1')};
+    font-size: 20px;
+    font-weight: normal;
+  }
+
+  span {
+    display: block;
+    margin-top: 3px;
+    color: ${({ available }) => (available ? '#999' : '#666')};
+  }
+`;
